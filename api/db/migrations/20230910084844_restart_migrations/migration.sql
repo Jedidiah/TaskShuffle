5d471d189ff7f029cf2722b18256dca961c0d82e
Jedidiah Broadbent <hello@jedidiah.dev>
2023-09-10 08:53:09 +0000

    Clear Migrations

--- api/db/migrations/20230910084844_restart_migrations/migration.sql
+++ api/db/migrations/20230910084844_restart_migrations/migration.sql
@@ -1,36 +1,44 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME
+);
+
+-- CreateTable
 CREATE TABLE "List" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT,
+    "title" TEXT NOT NULL,
     "description" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
-    "webhook" TEXT,
-    "url" TEXT,
     "isPrivate" BOOLEAN NOT NULL DEFAULT false,
     "skipLimit" INTEGER DEFAULT 0,
+    "order" TEXT NOT NULL DEFAULT '',
+    "theme" TEXT,
+    "themeProperties" TEXT,
+    "tags" TEXT NOT NULL DEFAULT '',
     CONSTRAINT "List_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "ListItem" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT,
+    "title" TEXT NOT NULL DEFAULT 'Untitled List',
     "description" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
     "listId" TEXT NOT NULL,
     "webhook" TEXT,
-    "listItemTagId" TEXT,
+    "url" TEXT,
+    "tags" TEXT DEFAULT '',
     CONSTRAINT "ListItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "ListItem_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "ListItem_listItemTagId_fkey" FOREIGN KEY ("listItemTagId") REFERENCES "ListItemTag" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    CONSTRAINT "ListItem_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
--- CreateTable
-CREATE TABLE "ListItemTag" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "listId" TEXT NOT NULL,
-    CONSTRAINT "ListItemTag_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
